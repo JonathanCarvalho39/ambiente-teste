346bf6f55b35d710b37661d3c66497f22105563c
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2024-03-11 00:12:50 -0300

    Atualizando site todo

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elementoHeader = document.getElementById('header'); 
-    let quantidadePixelsDoTopo = 10; 
+document.addEventListener('DOMContentLoaded', function () {
+    let elementoHeader = document.getElementById('header');
+    let quantidadePixelsDoTopo = 80;
     window.addEventListener('scroll', function () { // Escuta se a tela vai scrola ou não
         if (window.scrollY > quantidadePixelsDoTopo) {
             elementoHeader.classList.add('changeStyle');
@@ -9,3 +9,34 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+let interuptor = 1
+
+
+function sumirMenu() {
+    let menu = document.getElementById('menu');
+    let dash = document.getElementById('tela_principal');
+    let itens_menu = document.getElementById('itens_menu');
+    let seta = document.getElementById('seta');
+    let logo = document.getElementById('logotipo');
+
+    if (interuptor == 0) {
+        menu.style.width = '5vw'
+        dash.style.width = '95vw'
+        dash.style.marginLeft = '5vw'
+        itens_menu.style.display = 'none'
+        logo.style.display = 'none';
+        seta.style.rotate = '0deg'
+        interuptor = 1
+    } else {
+        menu.style.width = '20vw'
+        dash.style.width = '80vw'
+        dash.style.marginLeft = '20vw'
+        interuptor = 0
+        itens_menu.style.display = 'flex'
+        seta.style.rotate = '180deg'
+        logo.style.display = 'block';
+    }
+
+
+}
